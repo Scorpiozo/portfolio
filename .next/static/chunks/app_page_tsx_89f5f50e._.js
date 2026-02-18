c7aa2bbdafdbc34146997073e08e7e243e16b84b
Scorpiozo <anuakhilscorpio9@gmail.com>
2026-02-18 23:13:39 +0530

    chore: remove ignored files from tracking

--- .next/static/chunks/app_page_tsx_89f5f50e._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_6b141b2e._.js",
-  "static/chunks/app_64dc66f6._.js"
-],
-    source: "dynamic"
-});
